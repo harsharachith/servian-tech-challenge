df7efa07ea4479908889bf3850c05f11dd17bc7c
harsharachith <harsharachith@gmail.com>
2022-06-15 23:50:18 +0530

    added manual trigger

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
- backend "gcs" {
-   depends_on = ["google_storage_bucket.default"]
-   bucket  = "bucket-tfstatee"
-   prefix  = "terraform/state"
- }
-}
--- main.tf
+++
@@ -1,9 +0,0 @@
-resource "google_storage_bucket" "default" {
-  name          = "bucket-tfstatee"
-  force_destroy = false
-  location      = "US"
-  storage_class = "STANDARD"
-  versioning {
-    enabled = true
-  }
-}
